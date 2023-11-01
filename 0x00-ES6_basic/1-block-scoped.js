51646ac99c2ecdf9c453ca4659722d2c4f938aa0
vickouma77 <Vickprestone20@gmail.com>
2023-11-01 15:53:30 +0300

    hoisting variables

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+export default function taskBlock(trueOrFalse) {
+
+    if (trueOrFalse) {
+      var task = true;
+      var task2 = false;
+    }
+
+    var task = true;
+    var task2 = false;
+  
+    return [task, task2];
+  }
+  
