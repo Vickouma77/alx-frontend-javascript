e7bde0673ba65c03e417813555d462ebdab2f707
vickouma77 <Vickprestone20@gmail.com>
2023-11-03 18:31:19 +0300

    return a promise. The parameter is a boolean

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  if (success === true) {
+    return new Promise((resolve) => {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    });
+  }
+  return new Promise((reject) => {
+    reject(new Error('The fake API is not working currently'));
+  });
+}
