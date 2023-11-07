09b7eb7c6290a8f0d9a93056cc0701d04a0b10d7
vickouma77 <Vickprestone20@gmail.com>
2023-11-07 15:17:12 +0300

    Throw error/try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,7 @@
+/* eslint-disable */
 export default function divideFunction(numerator, denominator) {
-  try {
     if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    } else {
-      return numerator / denominator;
+      throw new Error("cannot divide by 0");
     }
-  } catch (error) {
-    return error.toString();
+    return numerator / denominator;
   }
-}
