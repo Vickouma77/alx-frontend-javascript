3cb19580a4c72d0362d102ed37eef58ba359b925
vickouma77 <Vickprestone20@gmail.com>
2023-11-07 14:20:38 +0300

    multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
-// eslint-disable-next-line no-unused-vars
+
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser, uploadPhoto])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((results) => {
-      for (const result of results) {
-        if (result.status === 'rejected') {
-          result.value = `${result.reason.name}: ${result.reason.message}`;
-          delete result.reason;
-          result.status = 'error';
-        }
-      }
-      return results;
+      const rejected = results.filter((result) => result.status === 'rejected');
+      const errors = rejected.map((result) => result.reason.toString());
+      const resolved = results.filter((result) => result.status === 'fulfilled');
+      const messages = resolved.map((result) => result.value.body);
+      return { errors, messages };
     });
 }
