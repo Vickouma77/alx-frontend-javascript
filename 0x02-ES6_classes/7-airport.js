3a174c788ce7618f8bcb5e1f652fdf61dcaa0d07
Vickouma77 <Vickprestone20>
2023-11-17 13:44:20 +0300

    7-airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-underscore-dangle */
 export default class Airport {
-    constructor(name, code) {
-        if (typeof name !== 'string') {
-            throw TypeError('name must be a string');
-        } else {
-            this._name = name;
-        }
-
-        if (typeof code !== 'string') {
-            throw TypeError('code must be a string');
-        } else {
-            this._code = code;
-        }
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw TypeError('name must be a string');
+    } else {
+      this._name = name;
     }
 
-    get [Symbol.toStringTag]() {
-        return this._code;
+    if (typeof code !== 'string') {
+      throw TypeError('code must be a string');
+    } else {
+      this._code = code;
     }
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
 }
