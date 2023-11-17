c763ef93a73e35e337b17bf88921de17d33a7b1d
Vickouma77 <Vickprestone20>
2023-11-17 13:39:41 +0300

    7-airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,19 @@
+export default class Airport {
+    constructor(name, code) {
+        if (typeof name !== 'string') {
+            throw TypeError('name must be a string');
+        } else {
+            this._name = name;
+        }
+
+        if (typeof code !== 'string') {
+            throw TypeError('code must be a string');
+        } else {
+            this._code = code;
+        }
+    }
+
+    get [Symbol.toStringTag]() {
+        return this._code;
+    }
+}
